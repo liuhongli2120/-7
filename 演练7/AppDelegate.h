cca440d9d3deb813d47a925610345dcadfd7403a
刘宏立 <liuhongli2120@163.com>
2016-06-23 23:50:05 +0800

    Initial Commit

---
+++ 演练7/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  演练7
+//
+//  Created by 刘宏立 on 16/6/23.
+//  Copyright © 2016年 lhl. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
